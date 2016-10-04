715e24d3c31cc63c4041e4e33e11716ccc5f4e21
rndmized <a.rndmized@gmail.com>
2016-10-04 15:59:11 +0100

    Changes to mywebapp.py and index.html

--- exercise_2/mywebapp.py
+++ exercise_2/mywebapp.py
@@ -9,5 +9,10 @@ def hello():
 def helloMe():
     return "Hello Albert!"
 
+@app.route('/<name>')
+def show_post(name):
+    # show the message with the user name, the name is a string
+    return 'Your name is %s' % name
+
 if __name__ == "__main__":
     app.run()
