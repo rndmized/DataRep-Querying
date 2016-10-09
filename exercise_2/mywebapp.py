16e9b4576958b15b7ca32b4a7c935a7c1acfc14e
rndmized <a.rndmized@gmail.com>
2016-10-09 16:08:59 +0100

    mywebappy takes name from http request and creates new page displaying submited name.

--- exercise_2/mywebapp.py
+++ exercise_2/mywebapp.py
@@ -1,18 +1,21 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
-@app.route("/albert")
+@app.route("/named/", methods=['GET'])
 def helloMe():
-    return "Hello Albert!"
+	 # show the message with the user name, the name is a string requested through the get method key is id, '' represents value.
+    return 'Your name is %s' % request.args.get('id', '')
 
-@app.route('/<name>')
-def show_post(name):
+#@app.route('/<name>')
+#def show_post(name):
     # show the message with the user name, the name is a string
-    return 'Your name is %s' % name
+ #   return 'Your name is %s' % name
 
 if __name__ == "__main__":
     app.run()
