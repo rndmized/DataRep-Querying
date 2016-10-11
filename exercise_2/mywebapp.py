58503d2f42afe32df0c93faf47542d339576ffcc
rndmized <a.rndmized@gmail.com>
2016-10-11 14:49:31 +0100

    POST method added, single-page refresh

--- exercise_2/mywebapp.py
+++ exercise_2/mywebapp.py
@@ -3,14 +3,26 @@ from flask import render_template
 from flask import request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('index.html')
+#@app.route("/")
+#def hello():
+#    return render_template('index.html')
 
-@app.route("/named/", methods=['GET'])
+#@app.route("/named/", methods=['GET', 'POST'])
+#def helloMe():
+	 # show the message with the user name, the name is a string requested through the get method key is id, '' represents value.
+ #   if request.method == 'POST':
+  #  	return 'Your name is %s' % request.form['id']
+   # return 'Your name is %s' % request.args.get('id', '')
+
+#nameInputed
+@app.route("/", methods=['GET', 'POST'])
 def helloMe():
 	 # show the message with the user name, the name is a string requested through the get method key is id, '' represents value.
-    return 'Your name is %s' % request.args.get('id', '')
+    if request.method == 'POST':
+    	return render_template('index.html', nameInputed =request.form['id'])
+    return render_template('index.html', nameInputed =request.args.get('id', ''))
+
+
 
 #@app.route('/<name>')
 #def show_post(name):
